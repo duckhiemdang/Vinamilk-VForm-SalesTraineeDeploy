f72c1c9373b8ae388598c6649790bf3efc3a7fa8
duckhiem2003 <duckhiem2003@gmail.com>
2025-08-14 16:36:33 +0700

    update vinamilk again

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "682330956"
+    serviceWorkerVersion: "3004302975"
   }
 });
