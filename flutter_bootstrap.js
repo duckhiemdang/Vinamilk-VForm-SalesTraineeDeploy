829c6125517978c4221580cd22495ed43612e799
duckhiem2003 <duckhiem2003@gmail.com>
2025-08-15 17:32:33 +0700

    uodate

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4050273445"
+    serviceWorkerVersion: "4208471888"
   }
 });
