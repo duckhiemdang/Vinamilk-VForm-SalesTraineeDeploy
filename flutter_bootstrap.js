0b1b32a217c0d6304b8f29e95c9b8d47e6dce926
duckhiem2003 <duckhiem2003@gmail.com>
2025-08-20 18:08:46 +0700

    update admin page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4208471888"
+    serviceWorkerVersion: "2314243242"
   }
 });
